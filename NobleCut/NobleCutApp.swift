6bd354bfd3c6a0ee51551d0f01f2c8ed083c1b06
ArtakTer-Stepanyan <artakterstepanyan974@gmail.com>
2026-04-22 23:32:20 +0400

    Initial Commit

---
+++ NobleCut/NobleCutApp.swift
@@ -0,0 +1,17 @@
+//
+//  NobleCutApp.swift
+//  NobleCut
+//
+//  Created by Artak Ter-Stepanyan on 22.04.26.
+//
+
+import SwiftUI
+
+@main
+struct NobleCutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
